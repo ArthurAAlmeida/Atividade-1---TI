75a18c6756df2332b2c5d20e5ef952ea28b2cf2f
ArthurAAlmeida <arturalves345@outlook.com>
2025-02-15 18:46:26 -0300

    Atividade 1

---
+++ Atividade1.java
@@ -0,0 +1,20 @@
+package Atividade1;
+import java.util.Scanner;
+
+
+public class Atividade1 {
+	
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N1 = sc.nextInt();
+		int N2 = sc.nextInt();
+		
+		System.out.println(N1 + N2);
+		
+		sc.close();
+	}
+
+}
